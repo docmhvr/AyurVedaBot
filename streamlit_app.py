d57e98be96a7dcfa4b2a6a7561b3af9dab3b776a
doc_mhvr <38325828+docmhvr@users.noreply.github.com>
2024-11-05 08:17:24 +0000

    Fixed icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if user_input := st.chat_input("Tell me about your ailment?"):
 
     # Stream the response to the chat using `st.write_stream`, then store it in 
     # session state.
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar="🌿"):
         response = st.write_stream(stream)    
 
     # Store the full response in session state to preserve chat history
